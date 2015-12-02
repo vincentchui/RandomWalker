19ff076b4a89ab8f6acd53365bf8edea8280a3bd
vincentchui <vincentchuicalifornia@gmail.com>
2015-12-02 09:48:41 -0800

    inital

---
+++ RandomWalker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomWalker
+{
+    class RandomWalker
+    {
+        public void randomWalk(char[,] maze)
+        {
+
+        }
+    }
+}
---
+++ RandomWalkerTester.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomWalker
+{
+    class RandomWalkerTester
+    {
+        static void Main(string[] args)
+        {
+            RandomWalker r = new RandomWalker();
+
+            char[,] maze = new char[10, 10];
+
+            for (int x = 0; x < 10; x++)
+            {
+                maze[x, z] = 'X';
+                
+            }
+        }
+    }
+}
